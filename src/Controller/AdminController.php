86646f8d92af3f4c432fbd899a3c8943752eb866
dogukan <dogukan_a@hotmail.nl>
2020-12-10 14:16:54 +0100

    2de commit

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route ("/", name="homepage")
+     */
+    public function homepageAction(){
+        return new response("<h1>Welkom<h1>");
+        return $this->render('admin/')
+    }
+
+}
